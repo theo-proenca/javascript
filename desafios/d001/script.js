f51840ac662fdf118b3a239d8813c592da82a5f4
Theo Proença <theoproenca@gmail.com>
2023-02-22 19:39:41 -0300

    desafio 01

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function b1(){
+    window.alert('Você clicou no botão 1!')
+}
+
+function b2(){
+    window.alert('Você clicou no botão 2!')
+}
+
+function b3(){
+    window.alert('Você clicou no botão 3!')
+}
