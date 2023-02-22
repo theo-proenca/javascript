199b3b891b072f0755af8e85fc9a3df36cb3916b
Theo Proença <theoproenca@gmail.com>
2023-02-22 20:14:33 -0300

    desafio2

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function start(){
+    var nome = window.prompt('Qual é o seu nome?')
+    var idade = window.prompt(`Olá, ${nome}! Quantos anos você tem?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
